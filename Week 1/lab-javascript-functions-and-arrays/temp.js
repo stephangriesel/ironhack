a0f36481e74768025f8a8d2f1a4ff2615da40794
Stephan Griesel <sgriesel@gmail.com>
2019-01-30 15:29:15 +0100

    nav done

---
+++ Week 1/lab-javascript-functions-and-arrays/temp.js
@@ -0,0 +1,220 @@
+/* 
+
+Find the maximum
+Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest.
+
+*/
+
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    console.log("the first is greater");
+  } else {
+    console.log("the second is greater");
+  }
+}
+
+maxOfTwoNumbers(92786, 876871);
+
+/* 
+
+Finding Longest Word
+Write a function findLongestWord that takes an array of words and returns the longest one. If there are 2 with the same length, it should return the first occurrence.
+
+*/
+
+var words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot"
+];
+
+var longest = 0;
+var longestWord = " ";
+
+for (var i = 0; i < words.length; i++) {
+  if (words[i].length > longest) {
+    longest = words[i].length;
+    longestWord = words[i];
+  }
+}
+
+console.log("The longest word is " + longestWord);
+
+/*
+
+Calculating a Sum
+Calculating a sum is as simple as iterating over an array and adding each of the elements together.
+
+Semantically reduce is the best method to use for this, but you can use any loop we’ve discussed so far.
+
+Create a sumArray function that takes an array of numbers as a parameter, and calculate the sum of all its numbers:
+
+Starter Code
+
+var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+*/
+
+var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+const add = (a, b) => a + b;
+
+const sum = numbers.reduce(add);
+console.log(sum);
+
+/* Calculate the Average
+Calculating an average is an extremely common task. Let’s practice it a bit.
+
+Algorithm
+
+Find the sum as we did in the first exercise
+Take the sum from step 1, and divide it by the number of elements in the list.
+Level 1: Array of Numbers
+Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers: */
+
+var numbers = [2, 6, 9, 10, 7, 4, 1, 9];
+let sum = numbers.reduce((previous, current) => (current += previous));
+let avg = sum / numbers.length;
+console.log(avg);
+
+/* Level 2: Array of Strings
+Write a function averageWordLength that receives an array of words and calculate the average length of the words:
+
+*/
+
+var words = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace"
+];
+
+let averageWordLength = numbers.reduce(
+  (previous, current) => (current += previous)
+);
+let avgWord = averageWordLength / words.length;
+console.log(avgWord);
+
+/* 
+
+Unique Arrays
+Take the following array, remove the duplicates, and return a new array. You’re more than likely going to want to check out the indexOf function.
+
+Do this in the form of a function uniquifyArray that receives an array of words as a parameter.
+
+*/
+
+var words = [
+  "crab",
+  "poison",
+  "contagious",
+  "simple",
+  "bring",
+  "sharp",
+  "playground",
+  "poison",
+  "communion",
+  "simple",
+  "bring"
+];
+
+let unique = [...new Set(words)];
+console.log(unique);
+
+/* 
+
+  Finding Elements
+Let’s create a simple array search.
+
+Write a function doesWordExist that will take in an array of words as one argument, and a word to search for as the other. Return true if it exists, otherwise, return false. Don’t use indexOf for this one. smile
+
+*/
+
+var words = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience"
+];
+
+var dict = {};
+
+function doesWordExist() {
+  words.forEach(function(word, i) {
+    dict[word] = i;
+  });
+}
+
+function search(term) {
+  if (term in dict) {
+    return console.log(true);
+    //console.log(term + ' is found at ' + dict[term]);
+  } else {
+    return console.log(false);
+  }
+}
+
+doesWordExist();
+search("truth");
+
+/* 
+
+Counting Repetion
+Write a function howManyTimes that will take in an array of words as one argument, and a word to search for as the other. The function will return the number of times that word appears in the array.
+
+*/
+
+function howManyTimes(original) {
+  var compressed = [];
+  var copy = original.slice(0);
+
+  for (var i = 0; i < original.length; i++) {
+    var myCount = 0;
+    for (var w = 0; w < copy.length; w++) {
+      if (original[i] == copy[w]) {
+        myCount++;
+        delete copy[w];
+      }
+    }
+
+    if (myCount > 0) {
+      var a = new Object();
+      a.value = original[i];
+      a.count = myCount;
+      compressed.push(a);
+    }
+  }
+
+  return compressed;
+}
+
+var words = new Array(
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter"
+);
+var newArray = howManyTimes(words);
+
+console.log(newArray);
