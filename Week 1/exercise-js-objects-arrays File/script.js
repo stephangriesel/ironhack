44bb75debe3c542ef184f7e7fe69be3c8f3ecaa2
Stephan Griesel <sgriesel@gmail.com>
2019-01-31 12:28:49 +0100

    two dim array

---
+++ Week 1/exercise-js-objects-arrays File/script.js
@@ -0,0 +1,11 @@
+var twoDim = 
+    [
+        ["Jan", "Koos", "Piet"],
+        ["Sannie", "Mari", "Lisbet"],
+        ["Nikki", "Pikki", "Tikki"]
+    ];
+
+for (var i = 0;i < twoDim.length;i++){
+    console.log(twoDim[0][0]);
+}
+
