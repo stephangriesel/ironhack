9196de4d998ab5a76d3ab6454532799574501c60
Stephan Griesel <sgriesel@gmail.com>
2019-02-04 16:34:04 +0100

    week 2

--- Week1/lab-dom-ironhack-cart/js/index.js
+++ Week1/lab-dom-ironhack-cart/js/index.js
@@ -1,4 +1,16 @@
 var product = document.getElementsByClassName("product");
 product[0].innerHTML = "IronBubble-Head";
-var product = document.getElementsByClassName("product");
-product[0].innerHTML = "IronBubble-Head";
+
+var total = document.getElementsByClassName("tota")
+
+// Iteration 1: Creating one product
+// We will start by creating the HTML for one of your products. It should look like this:
+
+// Every product will have:
+
+// A wrapper div that contains all the HTML for that product.
+// A div with a span, showing the product name.
+// A div with a span, showing the cost of one unit.
+// A div with one label and one input, where the user will indicate how many units they will buy.
+// A div with a span, showing the total price for this product. This number should be the result of multiplying the amount of units multiplied by the price of one unit of that product. The default total price should be 0.
+// A div with a delete button, to remove the product from the list.
---
+++ Week2/exercise-jquery/js/script.js
@@ -0,0 +1,22 @@
+// $(".element").click(function(event) {
+//     alert("DOM is Loaded");
+//   });
+
+  $("p").click(function() {
+      $(this).remove()
+  })
+
+  $("button").click(function() {
+      $("p").remove()
+  })
+
+  $(document).ready(function(){
+      $(".popup").click(function(event){
+          alert( "Hi");
+      });
+  });
+
+
+
+
+
