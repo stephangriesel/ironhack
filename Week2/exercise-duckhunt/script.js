555d619f00c63837bc18ce7364e0977bc8b80b9f
Stephan Griesel <sgriesel@gmail.com>
2019-02-06 12:16:39 +0100

    add pizzabuilder v2

---
+++ Week2/exercise-duckhunt/script.js
@@ -0,0 +1,22 @@
+$( "#hunter" ).keydown(function( event ) {
+    if ( event )
+    
+    // var item;
+
+    // switch(){
+    //     case "up":
+
+    //     break;
+    //     case "left":
+
+    //     break;
+    //     case "right":
+
+    //     break;
+    // }
+
+    alert( "Handler for .keydown() called." );
+  });
+
+  // https://api.jquery.com/keydown/
+
