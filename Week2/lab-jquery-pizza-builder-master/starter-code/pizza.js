2a4a682c037ef5ee73ad843521780e252108822d
Stephan Griesel <sgriesel@gmail.com>
2019-02-08 08:56:06 +0100

    pizza working v3 - prices not updated yet

--- Week2/lab-jquery-pizza-builder-master/starter-code/pizza.js
+++ Week2/lab-jquery-pizza-builder-master/starter-code/pizza.js
@@ -3,32 +3,35 @@
 $(".btn-mushrooms").on("click", function() {
   $(".mushroom").toggle("mushroom")
   $(this).toggleClass("active");
+  $("ul li:contains('mushrooms')").toggle( 1000 );
 });
 
 $(".btn-pepperonni").on("click", function() {
   $(".pep").toggle("pep");
   $(this).toggleClass("active");
+  $("ul li:contains('pepperonni')").toggle( 1000 );
 });
 
 $(".btn-green-peppers").on("click", function() {
   $(".green-pepper").toggle("green-pepper");
   $(this).toggleClass("active");
+  // remove li if clicked but how to add again, conditional?
+  $("ul li:contains('green')").toggle( 1000 );
 });
 
 $(".btn-sauce").on("click", function() {
   $(".sauce-white").toggle(".sauce-white");
   $(this).toggleClass("active");
-  $("ul li:contains('white')").hide();
-  // if class is active show price in summary but can I use conditional to hide again?
-  $("ul li:contains('white')").show( 1000 );
+  // $("ul li:contains('white')").hide();
+  // remove li if clicked but how to add again, conditional?
+  $("ul li:contains('white')").toggle( 1000 );
 });
 
  $(".btn-crust").on("click", function() {
   $(".crust-gluten-free").toggle("crust-gluten-free");
   $(this).toggleClass("active");
   $("section#main-crust").addClass("crust-gluten-free");
-  // if class is active show price in summary but can I use conditional to hide again?
-  $("ul li:contains('gluten')").show( 1000 );
+  $("ul li:contains('gluten')").toggle( 1000 );
 });
 
 // Show main crust
