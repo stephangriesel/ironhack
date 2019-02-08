1e6698246a2889ca27ed038fe702cd3ae5ab2d93
Stephan Griesel <sgriesel@gmail.com>
2019-02-08 14:00:35 +0100

    fix crust

--- Week2/lab-jquery-pizza-builder-master/starter-code/pizza.js
+++ Week2/lab-jquery-pizza-builder-master/starter-code/pizza.js
@@ -1,4 +1,13 @@
+var totalPrice = 10;
+
 // Buttons
+
+$(".btn-pepperonni").on("click", function() {
+  $(".pep").toggle("pep");
+  $(this).toggleClass("active");
+  $("ul li:contains('pepperonni')").toggle( 1000 );
+  // if class is active add 1 to totalPrice else subtract 1 to totalPrice
+});
 
 $(".btn-mushrooms").on("click", function() {
   $(".mushroom").toggle("mushroom")
@@ -6,31 +15,21 @@ $(".btn-mushrooms").on("click", function() {
   $("ul li:contains('mushrooms')").toggle( 1000 );
 });
 
-$(".btn-pepperonni").on("click", function() {
-  $(".pep").toggle("pep");
-  $(this).toggleClass("active");
-  $("ul li:contains('pepperonni')").toggle( 1000 );
-});
-
 $(".btn-green-peppers").on("click", function() {
   $(".green-pepper").toggle("green-pepper");
   $(this).toggleClass("active");
-  // remove li if clicked but how to add again, conditional?
   $("ul li:contains('green')").toggle( 1000 );
 });
 
 $(".btn-sauce").on("click", function() {
   $(".sauce-white").toggle(".sauce-white");
   $(this).toggleClass("active");
-  // $("ul li:contains('white')").hide();
-  // remove li if clicked but how to add again, conditional?
   $("ul li:contains('white')").toggle( 1000 );
 });
 
  $(".btn-crust").on("click", function() {
-  $(".crust-gluten-free").toggle("crust-gluten-free");
+  $("#main-crust").toggleClass("crust-gluten-free");
   $(this).toggleClass("active");
-  $("section#main-crust").toggle("crust-gluten-free");
   $("ul li:contains('gluten')").toggle( 1000 );
 });
 
@@ -38,9 +37,13 @@ $(".btn-sauce").on("click", function() {
 
 $("section#main-crust").removeClass("crust-gluten-free");
 
+// Add tomato sauce
+$(".sauce").addClass("sauce");
+
+
 // Hide white sauce & gluten free default
 
-$(".sauce-white").hide(); // this vs document ready on top?
+$(".sauce-white").hide();
 $(".crust-gluten-free").hide();
 
 // Change button state for items not added
@@ -48,10 +51,18 @@ $(".crust-gluten-free").hide();
 $("button#sauce").removeClass("active");
 $("button#gluten").removeClass("active");
 
+// Show tomato base 
+
+
+
 // Hide options in price summary that is not on the pizza
 
 $("ul li:contains('gluten')").hide();
 $("ul li:contains('white')").hide();
+
+// Hide/display crust
+
+
 
 
 
