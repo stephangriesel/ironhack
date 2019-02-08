8f566e5e87857bc5128b787c9a9c04546a7c7d8b
Stephan Griesel <sgriesel@gmail.com>
2019-02-08 14:06:51 +0100

    add memory game assignment

---
+++ Week2/lab-javascript-memory-game/starter-code/src/memory.js
@@ -0,0 +1,12 @@
+var MemoryGame = function (cards) {
+  this.cards = cards;
+};
+
+MemoryGame.prototype.shuffleCards = function () {
+};
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+}
+
+MemoryGame.prototype.isFinished = function () {
+};
