b54fd97363090fabd6111e6e5b5f13afe4a2bf9a
Stephan Griesel <sgriesel@gmail.com>
2019-02-12 19:03:55 +0100

    add exploding kip - buggy

--- Week3/Project/script.js
+++ Week3/Project/script.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const totalScore = document.getElementById("score"); // issue again with jquery
-  const cages = $(".cage");
+  const cages = document.querySelectorAll(".cage");
   const kips = document.querySelectorAll(".kip"); // could not get foreach working when using jquery syntax
   let lastCage; // Skip cage if same as previous
   let endGame = false; // If true game stops
   let score = 0;
   let difficulty = {
-    min:1000,
-    max:3000
+    min: 1000,
+    max: 3000
   }
 
   // Button click events
-  $("#start").click(function() {
+  $("#start").click(function () {
     beginGame();
   });
 
@@ -20,12 +20,11 @@ $(document).ready(function() {
     return Math.round(Math.random() * (max - min) + min);
   }
 
-  
   // Random cage
   function randomCage(cages) {
     const idx = Math.floor(Math.random() * cages.length);
     const cage = cages[idx];
-    if(cage === lastCage){ // Skip cage if same as previous
+    if (cage === lastCage) { // Skip cage if same as previous
       console.log("same cage, moving on...");
       return randomCage(cages);
     }
@@ -40,33 +39,32 @@ $(document).ready(function() {
     const cage = randomCage(cages);
     // console.log(time, cage);
     cage.classList.add("show");
-    setTimeout(function() {
+    setTimeout(function () {
       cage.classList.remove("show");
-      if(!endGame) popupKip(); // if true game stops
-    },time)
+      if (!endGame) popupKip(); // if true game stops
+    }, time)
   }
 
   // Begin game 
   function beginGame() {
-    // totalScore.textContent = 0;
     endGame = false;
     score = 0;
     popupKip();
-    setTimeout(function() {
+    setTimeout(function () {
       endGame = true
     }, 10000)
   }
 
   // Start game on spacebar keypress
-  window.onkeypress = function(event) {
+  window.onkeypress = function (event) {
     if (event.keyCode == 32) {
-       beginGame();
+      beginGame();
     }
- }
+  }
 
   // Whack kip and update score
   function whack(e) {
-    if(!e.isTrusted) return; // verify valid click
+    if (!e.isTrusted) return; // verify valid click
     //console.log(e);
     score++;
     // remove kip if clicked
@@ -75,29 +73,35 @@ $(document).ready(function() {
   }
 
   // Explode kip 
-  
-   
+  $(".kip").click(function(){
+    $(this).toggleClass("kipExplode");
+   })
+
 
   // Difficulty buttons
-  $("#easy-btn").click(function(){
+  $("#easy-btn").click(function () {
     $(".level-selected").html("Level Selected: Easy");
     difficulty.min = 3000;
     difficulty.max = 4000;
   })
 
-  $("#medium-btn").click(function(){
+  $("#medium-btn").click(function () {
     $(".level-selected").html("Level Selected: Medium");
     difficulty.min = 1500;
     difficulty.max = 2000;
   })
 
-  $("#hard-btn").click(function(){
+  $("#hard-btn").click(function () {
     $(".level-selected").html("Level Selected: Hard");
     difficulty.min = 500;
     difficulty.max = 900;
   })
 
   kips.forEach(kip => kip.addEventListener('click', whack)); // for loop
-    
-  });
+
+  // for(var i = 0; i < kips.length; i++) {
+
+  // }
+
+});
 
