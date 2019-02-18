945b340b42467e55423f5a8be0282d1af58de59b
Stephan Griesel <sgriesel@gmail.com>
2019-02-18 17:39:48 +0100

    add routes, update styling

--- Week4/lab-ironbeers/starter_code/app.js
+++ Week4/lab-ironbeers/starter_code/app.js
@@ -10,12 +10,28 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Partials
+hbs.registerPartials(__dirname + "/views/partials");
 
-
-app.get('/', (req, res, next) => {
+app.get('/index', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+
+  res.render('beers');
+});
+
+app.get('/randombeers', (req, res, next) => {
+
+  res.render('randombeers');
+});
+
+app.listen(3020);
+
+console.log("hello from beers")
 
 
-app.listen(3000);
+
+
+
