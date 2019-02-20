cbd4e81f142941a62cc467145fa4e44282b14a4e
Stephan Griesel <sgriesel@gmail.com>
2019-02-20 16:08:19 +0100

    inclass mongoose exercise

---
+++ Week4/inclass-mongoose/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const restaurantSchema = mongoose.Schema;
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true } );
+
+// Verify connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+// Define schema
+// var restaurants = new mongoose.Schema({
+//     address: Object,
+//     borough: string,
+//     cuisine: string,
+//     grades: Array,
+//     name: string,
+//     restaurant_id: string
+//   });
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
