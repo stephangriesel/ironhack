c47f1620924c9636f1edc8706fccf8768dc1cad0
Stephan Griesel <sgriesel@gmail.com>
2019-02-21 12:58:25 +0100

    after removing commments

--- Week4/lab-mongoose-movies-master/starter-code/app.js
+++ Week4/lab-mongoose-movies-master/starter-code/app.js
@@ -12,37 +12,6 @@ mongoose.connect('mongodb://localhost/moviesApp', { useNewUrlParser: true }, fun
     else console.log("Connected to port3000") // Research port variable
 });
 
-/*
-Mongoose documentation overview: Defining a model
-
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
- 
-const BlogPost = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
-});
-
-Source: https://www.npmjs.com/package/mongoose
-
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
- 
-const mySchema = new Schema({
-  name: String,
-  occupation: String,
-  catchPhrase: String
-});
-
-Accessing the model
-Once we define a model through mongoose.model('ModelName', mySchema), we can access it through the same function
-
-const Celebrity = mongoose.model('Celebrity', mySchema);
-
- */
-
 const port = app.listen(3000);
 
 
