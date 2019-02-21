3b884fe5e9f9af23a90586e7f500213fe9384e51
Stephan Griesel <sgriesel@gmail.com>
2019-02-21 11:43:19 +0100

    define model

--- Week4/lab-mongoose-movies-master/starter-code/app.js
+++ Week4/lab-mongoose-movies-master/starter-code/app.js
@@ -1,15 +1,45 @@
 var express = require('express')
 var app = express()
  
+// Define routes
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send( 'Test')
 })
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/exampleApp', { useNewUrlParser: true }, function (err) {
+mongoose.connect('mongodb://localhost/moviesApp', { useNewUrlParser: true }, function (err) {
     if (err) console.log("ERROR ERROR", err)
     else console.log("Connected")
 });
+
+// Mongoose documentation overview: Defining a model
+
+// const Schema = mongoose.Schema;
+// const ObjectId = Schema.ObjectId;
+ 
+// const BlogPost = new Schema({
+//   author: ObjectId,
+//   title: String,
+//   body: String,
+//   date: Date
+// });
+
+// Source: https://www.npmjs.com/package/mongoose
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+ 
+const mySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String
+});
+
+// Accessing the model
+// Once we define a model through mongoose.model('ModelName', mySchema), we can access it through the same function
+
+const Celebrity = mongoose.model('Celebrity', mySchema);
+
  
 app.listen(3000)
 
