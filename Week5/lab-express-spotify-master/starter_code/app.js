b0b5a98d290b17155fe7444811643ccd85a493e0
stephangriesel <sgriesel@gmail.com>
2019-02-28 11:01:05 +0100

    basic setup done

--- Week5/lab-express-spotify-master/starter_code/app.js
+++ Week5/lab-express-spotify-master/starter_code/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+var SpotifyWebApi = require('spotify-web-api-node');
 
 
 
@@ -11,6 +10,23 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+// Credentials - move credentials to config file
+const clientId = '358ec408641f4dbdb6e44e196262b599',
+    clientSecret = '36ae7e70fe7a48a185451c9c25bbaf61';
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 // setting the spotify-api goes here:
 
@@ -23,4 +39,4 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+app.listen(3010, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
