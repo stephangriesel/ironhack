04f492d2797c834fc16a853f08d65e9ae852c722
stephangriesel <sgriesel@gmail.com>
2019-03-11 07:26:09 +0100

    add week7 lab, set up basics

---
+++ Week7/exercises/react-module-day1-start-master/components/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function User(props) {
+  return <h2>Hello, {props.firstName}!</h2>;
+}
+
+export default User;
--- Week7/exercises/react-module-day1-start-master/src/App.js
+++ Week7/exercises/react-module-day1-start-master/src/App.js
@@ -1,14 +1,29 @@
+// App.js
 import React, { Component } from "react";
+// import the npm package's component
+import ReactPlayer from "react-player";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello Ironhackers! </h1>
-      </div>
-    );
-  }
+
+render() {
+  return (
+    <div className="App">
+      <User firstName="Harper"/>
+      <User firstName="Ana"/>
+
+      <ReactPlayer url="https://vimeo.com/channels/top/22439234" />
+
+      // see the new props!
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=kJQP7kiw5Fk"
+        playing
+        controls
+        volume="0.5"
+      />
+    </div>
+  );
+}
 }
 
 export default App;
