37b5973fa63bdbd678aae9aeed8fd4e312c1c7f8
stephangriesel <sgriesel@gmail.com>
2019-03-13 16:05:22 +0100

    todo delete working

--- Week7/inclass/myapp4/todo/src/components/Item.js
+++ Week7/inclass/myapp4/todo/src/components/Item.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Item extends Component {
     render() {
         return(
-            <h1>{this.props.name}</h1>
+            <h1 onClick={() => this.props.deleteItem(this.props.index)}>{this.props.name}</h1>
         )
     }
 }
--- Week7/inclass/myapp4/todo/src/components/ShoppingList.js
+++ Week7/inclass/myapp4/todo/src/components/ShoppingList.js
@@ -5,19 +5,25 @@ class ShoppingList extends Component {
 
     constructor() {
         super()
-        // this.updateShoppingList = this.updateShoppingList.bind(this)
+        this.updateShoppingList = this.updateShoppingList.bind(this)
     }
 
     state = {
         items: ["Milk", "Soya", "Legumes"]
     }
 
+    updateShoppingList(indexNumber){ // new function for updated shopping list
+        var newShoppingList = [...this.state.items] // define newShoppingList to safely splice it, avoid altering current state directly
+        newShoppingList.splice(indexNumber, 1) // splice array single item (1)
+        this.setState({items: newShoppingList}) // set state with new array 
+    }
 
     render() {
         return (
             <div>
                 {this.state.items.map((anItem, index) =>
-                    <Item name={anItem} />
+                    <Item name={anItem} index={index.toString()} deleteItem={this.updateShoppingList}/>
+
 
                 )}
             </div>
