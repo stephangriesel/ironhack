78ddc8c99713890685a543241a8b479cacf573e2
stephangriesel <sgriesel@gmail.com>
2019-03-19 17:21:31 +0100

    encrypt password - working version

--- Week8/inclass/forms-frontback/backend/routes/user-route.js
+++ Week8/inclass/forms-frontback/backend/routes/user-route.js
@@ -1,43 +1,72 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 //Models
 const User = require('../models/user');
 
-// 1. Route working
+// 1. TEST ROUTE
 // router.post('/users', (req, res, next) => {
 //     res.send('test');
 // });
 
-router.post('/users', (req, res, next) => {
-    User.create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(response => {
-            res.json(response);
-        })
-        .catch(err => {
-            res.json(err);
-        })
-});
+// SIGNUP
 
-// router.post('/newuser', (req, res, next) => {
-//     User.create({
-//       firstname: this.state.firstname,
-//       lastname: this.state.lastname,
-//       email: this.state.email,
-//       password: this.state.password
+router.post("/users", (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password;
+    if (firstname === "" || password === "") {
+      res.send("enter required fields please");
+      return;
+    } bcrypt.hash(password, saltRounds).then(hash => {
+      let newUser = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: hash
+      }
+  
+      return newUser;
+  
+    }).then(newUser => {
+      User.create(newUser)
+      res.send("success")
+    })
+      .catch(error => {
+        console.log(error);
+      })
+  });
+
+// LOGIN
+
+// router.post('/login', function (req, res) {
+//     User.findOne({ email: req.body.email }, function (err, user) {
+//       if (!user) {
+//         res.render('login', { error: 'Invalid credentials' });
+//       } else {
+//         if (bcrypt.compare(req.body.password, user.password)) {
+//           // set cookie with user data
+//           req.session.user = user;
+//           res.cookie("email", req.body.email, { signed: true }); 
+//           res.cookie("userId", user._id, { signed: true });     
+//           res.redirect('/books');
+//         } else {
+//           res.render('login', { error: 'Invalid credentials' })
+//         }
+//       }
 //     })
-//     .then(response => {
-//       res.json(response);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     })
+//   });
+  
+//   router.get('/login', (req, res) => {
+//     if (req.session.currentUser) {
+//       req.session((err) => {
+//         res.render('books', { newMessage: true })
+//       })
+//     } else res.render("login")     
 //   });
 
 module.exports = router;
--- Week8/inclass/forms-frontback/frontend/src/components/Form.js
+++ Week8/inclass/forms-frontback/frontend/src/components/Form.js
@@ -3,56 +3,66 @@ import axios from 'axios';
 
 class Form extends Component {
 
-    state = {
-        firstname: "",
-        lastname: "",
-        email: "",
-        password: ""
-      }
+  state = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: ""
+  }
 
-    handleChange = (event) => {
-        var updateChange = {};
-        updateChange[event.target.name] = event.target.value;
-        this.setState(updateChange);
-    }
-    
-    handleSubmit = (event) => {
-      const newUser = this.state;
-        event.preventDefault();
-        debugger
-        axios({
-            method: 'post',
-            url: 'http://localhost:3001/api/users',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              data: newUser,
-              withCredentials: true
-          }
-    
-          })
-          .then(function (response) {
-            console.log(response);
-            debugger
-          })
-          .catch(function (error) {
-            console.log(error);
-            // debugger
-          });
+  handleChange = (event) => {
+    var updateChange = {};
+    updateChange[event.target.name] = event.target.value;
+    this.setState(updateChange);
+  }
 
-    }
+  handleSubmit = (event) => {
+    const newUser = this.state;
+    event.preventDefault();
 
-    render() {
-        return (
-            
-            <form onSubmit={this.handleSubmit}>
-                <input onChange={event => this.handleChange(event)} name="firstname" placeholder="name" type="text" value={this.state.firstname}></input>
-                <input onChange={event => this.handleChange(event)} name="lastname" placeholder="lastname" type="text" value={this.state.lastname}></input>
-                <input onChange={event => this.handleChange(event)} name="email" placeholder="email" type="text" value={this.state.email}></input>
-                <input onChange={event => this.handleChange(event)} name="password" placeholder="password" type="password" value={this.state.password}></input>
-                <input type="submit"></input>
-            </form>
-        )
-    }
+    axios({
+      method: 'post',
+      url: 'http://localhost:3001/api/users',
+      data: newUser,
+      withCredentials: true,
+      //   headers: {
+      //     'Content-Type': 'application/x-www-form-urlencoded',
+
+
+      // }
+
+
+    })
+      .then(function (response) {
+        console.log(response);
+        // debugger
+      })
+      .catch(function (error) {
+        console.log(error);
+        // debugger
+      });
+
+  }
+
+  render() {
+    return (
+
+      <div><h2>Register</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={event => this.handleChange(event)} name="firstname" placeholder="name" type="text" value={this.state.firstname}></input>
+          <input onChange={event => this.handleChange(event)} name="lastname" placeholder="lastname" type="text" value={this.state.lastname}></input>
+          <input onChange={event => this.handleChange(event)} name="email" placeholder="email" type="text" value={this.state.email}></input>
+          <input onChange={event => this.handleChange(event)} name="password" placeholder="password" type="password" value={this.state.password}></input>
+          <input type="submit"></input>
+        </form>
+        <h2>Login</h2>
+        <form onSubmit={this.handeLogin}>
+          <input onChange={event => this.handleChange(event)} name="emaillogin" placeholder="please use email to login" type="text" value={this.state.emaillogin}></input>
+          <input onChange={event => this.handleChange(event)} name="passwordlogin" placeholder="enter your password" type="password" value={this.state.passwordlogin}></input>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Form;
