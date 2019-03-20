a8067865ec32f0f9b6b504cf1f7091ff50225ee8
stephangriesel <sgriesel@gmail.com>
2019-03-20 19:28:41 +0100

    working version, next step import browser router

--- Week8/project-management-app/app.js
+++ Week8/project-management-app/app.js
@@ -8,7 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const cors         = require('cors');
 
 
 // WHEN INTRODUCING USERS DO THIS:
@@ -67,7 +67,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 // ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
-
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+}));
 
 
 // ROUTES MIDDLEWARE STARTS HERE:
